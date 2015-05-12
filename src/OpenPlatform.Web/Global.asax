db576d933db341d1278174e215e129e7f3d6ab82
董兵 <dongbing@hujiang.com>
2015-05-12 09:58:46 +0800

    init

---
+++ src/OpenPlatform.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenPlatform.Web.MvcApplication" Language="C#" %>
---
+++ src/OpenPlatform.WebModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPlatform.WebModels
+{
+    public class Class1
+    {
+    }
+}
