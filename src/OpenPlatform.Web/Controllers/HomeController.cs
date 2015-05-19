40c18aad3f19b9cea23c16adea25334ee40455d2
Will Duan <dqywill@outlook.com>
2015-05-19 18:36:42 +0800

    添加遗漏文件

---
+++ src/OpenPlatform.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenPlatform.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var result = new ContentResult();
+            result.Content = Url.Content("~/app/index.html");
+            result.ContentType = "text/html";
+            return result;
+        }
+    }
+}
---
+++ src/OpenPlatform.Web/Controllers/MvcController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenPlatform.Web.Controllers
+{
+    public class MvcController : Controller
+    {
+        // GET: Mvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
